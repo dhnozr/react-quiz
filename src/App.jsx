4ccbfab96bb0b72a50dddbba1de582dda388cf2f
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-05-06 11:43:45 +0300

    implemented starting and answering function inside reducer

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Loader from './components/Loader';
 import Error from './components/Error';
+import StartScreen from './components/StartScreen';
+import Question from './components/Question';
+import NextButton from './components/NextButton';
 
 const initialState = {
   questions: [],
-  // loading, ready,finished,error gibi durumlari yonetecegim
+  // loading, ready,finished,error for handling state
   status: 'loading',
+  // index for tracking which question !start with the first question
+  index: 0,
+  // user answer
+  answer: null,
 };
 
 function App() {
@@ -19,13 +26,23 @@ function App() {
         break;
       case 'dataFailed':
         return { ...state, status: 'error' };
+        break;
+      case 'start':
+        return { ...state, status: 'active' };
+        break;
+      case 'answer':
+        return { ...state, answer: action.payload };
+        break;
+      case 'nextQuestion':
+        return { ...state, index: state.index + 1, answer: null };
       default:
         break;
     }
   };
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer }, dispatch] = useReducer(reducer, initialState);
 
-  console.log(questions);
+  // take how many questions in the array
+  const numOfQuestions = questions.length;
 
   // fetch questions
   useEffect(() => {
@@ -48,6 +65,13 @@ function App() {
         <Main>
           {status === 'loading' && <Loader />}
           {status === 'error' && <Error />}
+          {status === 'ready' && <StartScreen numOfQuestions={numOfQuestions} dispatch={dispatch} />}
+          {status === 'active' && (
+            <>
+              <Question question={questions[index]} answer={answer} dispatch={dispatch} />
+              <NextButton dispatch={dispatch} />
+            </>
+          )}
         </Main>
       </div>
     </>
