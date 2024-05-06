a7428e1a71f04f19e7541a2fc29d38f2968ecc66
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-05-06 14:46:13 +0300

    button created for updating question and finishing the game

--- src/components/NextButton.jsx
+++ src/components/NextButton.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const NextButton = ({ dispatch }) => {
+const NextButton = ({ dispatch, numOfQuestions, index }) => {
+  let actionType = 'nextQuestion';
+
+  if (index === numOfQuestions - 1) actionType = 'finish';
+
   return (
-    <button className='btn btn-ui' onClick={() => dispatch({ type: 'nextQuestion' })}>
-      Next Question
+    <button className='btn btn-ui' onClick={() => dispatch({ type: actionType })}>
+      {actionType === 'nextQuestion' ? 'Next' : 'Finish'}
     </button>
   );
 };
