3ecb0a44f7952031714df8a30972d709a0c08f8a
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-05-06 16:16:54 +0300

    lottie animation added

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import animation from '../../Animation - 1714999936586.json';
+import Lottie from 'lottie-react';
 
 const Loader = () => {
   return (
     <div className='loader-container'>
-      <div className='loader'></div>
-      <p>Loading questions...</p>
+      {/* <div className='loader'></div> */}
+      <span className='animation-wrapper'>
+        <Lottie animationData={animation} />
+      </span>
     </div>
   );
 };
