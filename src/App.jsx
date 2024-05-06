60614a96effba52ebb394e2bda2c4e81795d34ba
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-05-06 14:47:43 +0300

    function added to reduce function

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Error from './components/Error';
 import StartScreen from './components/StartScreen';
 import Question from './components/Question';
 import NextButton from './components/NextButton';
+import Progress from './components/Progress';
+import FinishScreen from './components/FinishScreen';
 
 const initialState = {
   questions: [],
@@ -15,6 +17,10 @@ const initialState = {
   index: 0,
   // user answer
   answer: null,
+  // user points
+  points: 0,
+  // keep max score of user
+  maxPoint: 0,
 };
 
 function App() {
@@ -31,18 +37,36 @@ function App() {
         return { ...state, status: 'active' };
         break;
       case 'answer':
-        return { ...state, answer: action.payload };
+        const correctAnswer = state.questions[state.index].correctOption;
+
+        return {
+          ...state,
+          answer: action.payload,
+          points: action.payload === correctAnswer ? state.points + state.questions[state.index].points : state.points,
+        };
         break;
       case 'nextQuestion':
         return { ...state, index: state.index + 1, answer: null };
+        break;
+      case 'finish':
+        return {
+          ...state,
+          status: 'finished',
+          maxPoint: state.points > state.maxPoint ? state.points : state.maxPoint,
+        };
+        break;
+      case 'restart':
+        return { ...initialState, questions: state.questions, status: 'ready', maxPoint: state.maxPoint };
       default:
         break;
     }
   };
-  const [{ questions, status, index, answer }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points, maxPoint }, dispatch] = useReducer(reducer, initialState);
 
   // take how many questions in the array
   const numOfQuestions = questions.length;
+  // calc maxPoints in the game
+  const maxPossiblePoint = questions.reduce((acc, curr) => acc + curr.points, 0);
 
   // fetch questions
   useEffect(() => {
@@ -68,9 +92,18 @@ function App() {
           {status === 'ready' && <StartScreen numOfQuestions={numOfQuestions} dispatch={dispatch} />}
           {status === 'active' && (
             <>
+              <Progress
+                numOfQuestions={numOfQuestions}
+                index={index}
+                maxPossiblePoint={maxPossiblePoint}
+                points={points}
+              />
               <Question question={questions[index]} answer={answer} dispatch={dispatch} />
-              <NextButton dispatch={dispatch} />
+              <NextButton dispatch={dispatch} numOfQuestions={numOfQuestions} index={index} />
             </>
+          )}
+          {status === 'finished' && (
+            <FinishScreen point={points} maxPossiblePoint={maxPossiblePoint} dispatch={dispatch} maxPoint={maxPoint} />
           )}
         </Main>
       </div>
