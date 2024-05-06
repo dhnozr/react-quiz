715013b4bfcc07745039d691793fbcbf9841cc71
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-05-06 10:48:06 +0300

    questinons fetched with useReducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
+
+const initialState = {
+  questions: [],
+  // loading, ready,finished gibi durumlari yonetecegim
+  status: 'loading',
+};
 
 function App() {
+  // reducer function
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'dataReceived':
+        return { ...state, questions: action.payload };
+        break;
+
+      default:
+        break;
+    }
+  };
+  const [{ questions }, dispatch] = useReducer(reducer, initialState);
+
+  console.log(questions);
+
+  // fetch questions
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = await fetch('/data.json');
+        const res = await data.json();
+        dispatch({ type: 'dataReceived', payload: res.questions });
+      } catch (error) {
+        dispatch({ type: 'dataFailed' });
+      }
+    };
+
+    getData();
+  }, []);
   return <></>;
 }
 
